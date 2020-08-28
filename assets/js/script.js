2fef15de8a89138578390e5e1684f62e4b371de1
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2020-08-28 03:00:17 -0700

    convert data to text

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,56 @@ var cityInputEl = document.querySelector("#search-city");
 
 var userFormEl = document.querySelector("#form-group");
 
+var currentCityEl = document.querySelector("#current-city");
+
+var temperatureEl = document.querySelector("#temperature");
+
+var humidityEl = document.querySelector("#humidity");
+
+var windSpeedEl = document.querySelector("#wind-speed");
+
+var uvIndexEl = document.querySelector("#uv-index")
+
+
+
+
+
 var getWeatherInfo = function(city){
     // format the weather api url
-    var apiUrl ="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=d55a30c7fcf4dbc07c554680997e50c7"
+    var apiKey = "d55a30c7fcf4dbc07c554680997e50c7";
+    var apiUrl ="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey 
+    
     fetch(apiUrl).then(function(response){
-    response.json().then(function(data){
-        console.log(data);
-    })
-     
+        if(response.ok){
+        response.json().then(function(data){
+
+            console.log(data);
+            //City name
+        var currentCityValue = data ['name'];
+        currentCityEl.textContent = currentCityValue;
+            //temprature
+        var temperatureValue = data ['main']['temp']     
+        var tempF = Math.round((temperatureValue - 273.15) * 1.80 + 32 );
+        temperatureEl.textContent = tempF;
+            //Humidity
+        var humidityValue = data ['main']['humidity'];
+        humidityEl.textContent = humidityValue;
+            //Wind Speed
+        var windSpeedValue = data ['wind']['speed'];
+        var windsmph=(windSpeedValue*2.237).toFixed(1);
+        windSpeedEl.textContent = windsmph;
+            //UV index
+            
+         })   
+        }else{
+            alert("Error"+ response.statusText);
+        }
+        
     })
 
 }
+
+
 
 
 
