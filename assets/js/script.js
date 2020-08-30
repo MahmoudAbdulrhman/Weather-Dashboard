9353940bb0104cdc7276fd08e9ce0b8b09799dd1
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2020-08-30 03:40:13 -0700

    add weather condition Icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
+var buttonContainer = document.querySelector(".list-group");
 
-var searchBtn = document.querySelector("#search-button")
+var laodButtonEl = document.querySelector("#loadButton")
+
+var searchBtn = document.querySelector("#search-button");
 
 var cityInputEl = document.querySelector("#search-city");
 
@@ -13,7 +16,13 @@ var humidityEl = document.querySelector("#humidity");
 
 var windSpeedEl = document.querySelector("#wind-speed");
 
-var uvIndexEl = document.querySelector("#uv-index")
+var uvIndexEl = document.querySelector("#uv-index");
+
+var currentweatherEl = document.querySelector("#current-weather");
+
+var iconEl = document.querySelector("#icon");
+
+
 
 // Forecast Var
 var nextDayDate = document.querySelector("#fDate0")
@@ -62,119 +71,156 @@ var saveCity = function (citySave) {
 }
 
 
+
 var getWeatherInfo = function (city) {
+
+
     // format the weather api url
 
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
 
+
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
+                console.log(data);
+                
+                //condition Img
+                var weathericon = data.weather[0].icon;
+                var iconUrl = "http://openweathermap.org/img/wn/" + weathericon + ".png"
+                iconEl.setAttribute("src" , (iconUrl));
 
-               
+
                 //City name
-               
                 var currentCityValue = data['name'];
                 currentCityEl.textContent = currentCityValue;
-                
+
                 //temprature
-                
+
                 var temperatureValue = data['main']['temp']
                 var tempF = Math.round((temperatureValue - 273.15) * 1.80 + 32);
-                temperatureEl.textContent = tempF;
-               
+                temperatureEl.innerHTML = tempF + " &deg;F" ;
+
                 //Humidity
-               
+
                 var humidityValue = data['main']['humidity'];
-                humidityEl.textContent = humidityValue;
-                
+                humidityEl.textContent = humidityValue + "%";
+
                 //Wind Speed
-                
+
                 var windSpeedValue = data['wind']['speed'];
                 var windsmph = (windSpeedValue * 2.237).toFixed(1);
-                windSpeedEl.textContent = windsmph;
-                
+                windSpeedEl.textContent = windsmph + " mph ";
+
                 //forecast
-                
+
                 var fiveApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
                 fetch(fiveApi).then(function (response) {
                     response.json().then(function (data) {
                         console.log(data);
-                         //next day information
+                        //next day information
                         var nextDate = (data.list[0].dt_txt)
-                        var date = nextDate.substr(0,10);
+                        var date = nextDate.substr(0, 10);
                         nextDayDate.textContent = date
 
+                        var img0El = document.querySelector("#wImg0");
+                        img0El.setAttribute("src", (iconUrl));
+
+
                         var nextDay = (data.list[0].main.temp);
-                        var temprF = Math.round((nextDay - 273.15) * 1.80 + 32);
-                        nextDayTemp.textContent = temprF;
+                        var temprF = Math.round((nextDay - 273.15) * 1.80 + 32 );
+                        nextDayTemp.innerHTML = temprF + " &deg;F"  ;
 
                         var nextHumidity = (data.list[0].main.humidity);
-                        nextDayHumidity.textContent = (nextHumidity);
-                       
-                         //secned day information
-                         var secDate = (data.list[5].dt_txt)
-                         var date1 = secDate.substr(0,10);
-                         secDayDate.textContent = date1
+                        nextDayHumidity.textContent = (nextHumidity) + "%";
 
-                         var secDay = (data.list[5].main.temp);
-                         var temprF2 = Math.round((secDay - 273.15) * 1.80 + 32);
-                         secDayTemp.textContent = temprF2;
- 
-                         var secHumidity = (data.list[5].main.humidity);
-                         secDayHumidity.textContent = (secHumidity);
+                        //secned day information
+                        var secDate = (data.list[5].dt_txt)
+                        var date1 = secDate.substr(0, 10);
+                        secDayDate.textContent = date1
 
-                          //thred day information
+                        var img1El = document.querySelector("#wImg1");
+                        img1El.setAttribute("src", (iconUrl));
+
+
+                        var secDay = (data.list[5].main.temp);
+                        var temprF2 = Math.round((secDay - 273.15) * 1.80 + 32);
+                        secDayTemp.innerHTML = temprF2 +" &deg;F" ;
+
+                        var secHumidity = (data.list[5].main.humidity);
+                        secDayHumidity.textContent = (secHumidity) + "%";
+
+                        //thred day information
                         var thrdDate = (data.list[13].dt_txt)
-                        var date2 = thrdDate.substr(0,10);
+                        var date2 = thrdDate.substr(0, 10);
                         thrdDayDate.textContent = date2
+
+                        var img2El = document.querySelector("#wImg2");
+                        img2El.setAttribute("src", (iconUrl));
 
                         var thrdDay = (data.list[13].main.temp);
                         var temprF3 = Math.round((thrdDay - 273.15) * 1.80 + 32);
-                        thrdDayTemp.textContent = temprF3;
+                        thrdDayTemp.innerHTML = temprF3 +" &deg;F" ;
 
                         var thrdHumidity = (data.list[13].main.humidity);
-                        thrdDayHumidity.textContent = (thrdHumidity);
+                        thrdDayHumidity.textContent = (thrdHumidity) + "%";
 
-                         //forth day information
+                        //forth day information
                         var forthDate = (data.list[21].dt_txt)
-                        var date3 = forthDate.substr(0,10);
+                        var date3 = forthDate.substr(0, 10);
                         forthDayDate.textContent = date3
 
-                         var forthDay = (data.list[21].main.temp);
-                         var temprF4 = Math.round((forthDay - 273.15) * 1.80 + 32);
-                         forthDayTemp.textContent = temprF4;
- 
-                         var forthHumidity = (data.list[21].main.humidity);
-                         forthDayHumidity.textContent = (forthHumidity);
+                        var img3El = document.querySelector("#wImg3");
+                        img3El.setAttribute("src", (iconUrl));
 
-                          //fifth day information
+
+                        var forthDay = (data.list[21].main.temp);
+                        var temprF4 = Math.round((forthDay - 273.15) * 1.80 + 32);
+                        forthDayTemp.innerHTML = temprF4 +" &deg;F" ;
+
+                        var forthHumidity = (data.list[21].main.humidity);
+                        forthDayHumidity.textContent = (forthHumidity) + "%";
+
+                        //fifth day information
                         var fifthDate = (data.list[21].dt_txt)
-                        var date4 = fifthDate.substr(0,10);
+                        var date4 = fifthDate.substr(0, 10);
                         fifthDayDate.textContent = date4
+
+                        var img4El = document.querySelector("#wImg4");
+                        img4El.setAttribute("src", (iconUrl));
+
 
                         var fifthDay = (data.list[4].main.temp);
                         var temprF5 = Math.round((fifthDay - 273.15) * 1.80 + 32);
-                        fifthDayTemp.textContent = temprF5;
+                        fifthDayTemp.innerHTML = temprF5 + " &deg;F";
 
                         var fifthHumidity = (data.list[4].main.humidity);
-                        fifthDayHumidity.textContent = (fifthHumidity);
-                       
+                        fifthDayHumidity.textContent = (fifthHumidity) + "%";
+
                     })
                 })
-            
-                
+
+
 
             })
         } else {
             alert("Error" + response.statusText);
         }
-       
+
     })
+
+    cityButton()
 
 }
 
+var cityButton = function () {
+    //create clickable button 
 
+    var btn = document.createElement("BUTTON");   // Create a <button> element
+    btn.setAttribute("id", "loadButton");
+    btn.innerHTML = cityInputEl.value.trim();                   // Insert text
+    buttonContainer.appendChild(btn);               // Append <button> to <body>
+}
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -192,6 +238,8 @@ var formSubmitHandler = function (event) {
     }
 
 };
+
+
 
 userFormEl.addEventListener("submit", formSubmitHandler);
 
